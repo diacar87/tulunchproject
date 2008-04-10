10a22c026a25246fb845d4a4d45b8b7bfa3bb366
yohan8829 <yohan8829@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-10 04:30:55 +0000

    Variable Yohan, ese soy yo.

--- Prueba2/src/prueba2/Main.java
+++ Prueba2/src/prueba2/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         // Segunda prueba del dia de hoy
         int contador =1;
+        boolean yohan = true;
         //tercera Prueba
         //Yohan canson...
         String developerMember = "Acactown";
         double tati;
         developerMember = "que paso Yohan";
+
     }
  }
