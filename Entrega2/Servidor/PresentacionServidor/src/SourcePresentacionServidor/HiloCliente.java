322dd63ae0a6dad6c8519fa8c93cec0b637e0948
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-20 06:29:22 +0000

    Final..hasta que me canse..JaJaJa

--- Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/HiloCliente.java
+++ Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/HiloCliente.java
@@ -6,8 +6,7 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Vector;
 import SourceLogicaNegocioServidor.LogServidor;
-import vo.*;
-import dao.*;
+
 
 
 /**
@@ -31,7 +30,55 @@ public class HiloCliente implements Runnable{
      }
      
      public void run(){
-         
+         /*LogServidor.setEvento("Esperando Peticiones del Cliente :"+ this);
+        try{
+            entradaComunicacion = new DataInputStream(skClienteComunicacion.getInputStream());
+            salida=new DataOutputStream(skClienteComunicacion.getOutputStream());
+            salida2=new DataOutputStream(scli2.getOutputStream());
+            this.setNameUser(entrada.readUTF());
+            enviaUserActivos();
+        }
+    	catch (IOException e) {  e.printStackTrace();     }
+    	
+        int opcion=0,numUsers=0;
+        String amigo="",mencli="";
+                
+    	while(true)
+    	{
+          try
+          {
+             opcion = entradaComunicacion.readInt();
+             switch(opcion)
+             {
+                case 1://envio de mensage a todos
+                   mencli = entradaComunicacion.readUTF();
+                   serv.mostrar("mensaje recibido "+mencli);
+                   enviaMsg(mencli);
+                   break;
+                case 2://envio de lista de activos
+                   numUsers=clientesActivos.size();
+                   salida.writeInt(numUsers);
+                   for(int i=0;i<numUsers;i++)
+                      salida.writeUTF(clientesActivos.get(i).nameUser);
+                   break;
+                case 3: // envia mensage a uno solo
+                   amigo = entradaComunicacion.readUTF();//captura nombre de amigo
+                   mencli= entradaComunicacion.readUTF();//mensage enviado
+                   enviaMsg(amigo,mencli);
+                   break;
+             }
+          }
+          catch (IOException e) {System.out.println("El cliente termino la conexion");break;}
+    	}
+    	serv.mostrar("Se removio un usuario");
+    	clientesActivos.removeElement(this);
+    	try
+    	{
+          serv.mostrar("Se desconecto un usuario");
+          scli.close();
+    	}	
+        catch(Exception et)
+        {serv.mostrar("no se puede cerrar el socket");}  */        
      }
      
 
