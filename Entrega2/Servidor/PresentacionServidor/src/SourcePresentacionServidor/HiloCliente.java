399ca3cbbe930c037869e869cb36c8ed3dfb333c
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-20 04:23:51 +0000

    Se unen todos los proyectos del servidor se organiza adecuadamente...

--- Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/HiloCliente.java
+++ Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/HiloCliente.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Vector;
-import SourceLogicaNegocioServidor.*;
+import SourceLogicaNegocioServidor.LogServidor;
 import vo.*;
 import dao.*;
 
--- Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/Servidor.java
+++ Entrega2/Servidor/PresentacionServidor/src/SourcePresentacionServidor/Servidor.java
@@ -1,22 +1,49 @@
 
 package SourcePresentacionServidor;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import SourceLogicaNegocioServidor.LogServidor;
+
+
+
 /**
  *
  * @author acactown
  */
-import java.io.*;
-import java.net.*;
+public class Servidor implements Runnable{
+    
+    boolean escuchando = true;
+    
+    public Servidor() {
+        LogServidor.setSalida();
+    }
 
-public class Servidor{
-   
-   EjecutarServidor ejecutarServidor;
-   public Servidor(){
-        ejecutarServidor = new EjecutarServidor();
-        Thread correrServidor = new Thread(ejecutarServidor);
-   }
+    public void run(){
+        ServerSocket skServerComunicacion = null;
+        ServerSocket skServerMensajes = null;
 
-   public static void main(String abc[]) throws IOException {                
-        Servidor servidor = new Servidor();
-   }  
+        try{
+            skServerComunicacion = new ServerSocket(8081);
+            skServerMensajes = new ServerSocket(8082);
+            LogServidor.setEvento("El Servidor esta Corriendo.");
+            while( escuchando ){
+                Socket skComunicacion = null;
+                Socket skMensajes = null;
+                try {
+                    LogServidor.setEvento("Esperando Cliente.");
+                    skComunicacion = skServerComunicacion.accept();
+                    skMensajes = skServerMensajes.accept();
+                } catch (IOException e){
+                    LogServidor.setEvento("Error 2: Falla de Comunicacion: " + skServerComunicacion + "\n    Causa :" + e.getMessage());
+                    continue;
+                }
+                HiloCliente cliente = new HiloCliente(skComunicacion, skMensajes, this);           
+                Thread correrCliente = new Thread(cliente);
+            }
+        }catch(IOException e){
+            LogServidor.setEvento("Error 1: Falla de Comunicacion: " + skServerComunicacion + "\n    Causa :" + e.getMessage());
+        }
+    }
 }
