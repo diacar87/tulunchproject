e6f82a01c399aee0a0490a99486f63ddbcce2b37
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-20 05:13:06 +0000

    Se implenta patron strategy para las peticionesV4

---
+++ Entrega2/Servidor/LogicaNegocioServidor/src/SourceLogicaNegocioServidor/PedirEmpleado.java
@@ -0,0 +1,17 @@
+
+package SourceLogicaNegocioServidor;
+
+import vo.Empleado;
+import dao.DaoEmpleado;
+
+/**
+ *
+ * @author acactown
+ */
+public class PedirEmpleado implements ITipoPeticion{
+
+    public void peticion() {
+        
+    }
+
+}
