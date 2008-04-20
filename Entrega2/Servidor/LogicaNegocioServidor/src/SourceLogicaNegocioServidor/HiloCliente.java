7f3007649ca317633b9e45dc64f5e18346935e49
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-20 03:35:54 +0000

    se implementa sockets V2

---
+++ Entrega2/Servidor/LogicaNegocioServidor/src/SourceLogicaNegocioServidor/HiloCliente.java
@@ -0,0 +1,146 @@
+
+package SourceLogicaNegocioServidor;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Vector;
+
+/**
+ *
+ * @author acactown
+ */
+public class HiloCliente implements Runnable{
+
+    Socket skClienteComunicacion=null;
+    Socket skClienteMensajes=null;
+    ObjectInputStream entradaComunicacion=null;
+    ObjectOutputStream salidaComunicacion=null;
+    ObjectOutputStream salidaMensajes=null;
+    public static Vector<HiloCliente> clientesActivos = new Vector();	
+    String nameUser;
+    Servidor serv;
+     
+     public threadServidor(Socket scliente,Socket scliente2,Servidor serv)
+     {
+        skClienteComunicacion=scliente;
+        skClienteMensajes=scliente2;
+        this.serv=serv;
+        nameUser="";
+        clientesActivos.add(this);        
+        serv.mostrar("cliente agregado: "+this);			
+     }
+     
+     public String getNameUser()
+     {
+       return nameUser;
+     }
+     
+     public void setNameUser(String name)
+     {
+       nameUser=name;
+     }
+     
+     public void run()
+     {
+    	serv.mostrar(".::Esperando Mensajes :");
+    	
+    	try
+    	{
+          entradaComunicacion=new DataInputStream(skClienteComunicacion.getInputStream());
+          salidaComunicacion=new DataOutputStream(skClienteComunicacion.getOutputStream());
+          salidaMensajes=new DataOutputStream(skClienteMensajes.getOutputStream());
+          this.setNameUser(entradaComunicacion.readUTF());
+          enviaUserActivos();
+    	}
+    	catch (IOException e) {  e.printStackTrace();     }
+    	
+        int opcion=0,numUsers=0;
+        String amigo="",mencli="";
+                
+    	while(true)
+    	{
+          try
+          {
+             opcion=entradaComunicacion.readInt();
+             switch(opcion)
+             {
+                case 1://envio de mensage a todos
+                   mencli=entradaComunicacion.readUTF();
+                   serv.mostrar("mensaje recibido "+mencli);
+                   enviaMsg(mencli);
+                   break;
+                case 2://envio de lista de activos
+                   numUsers=clientesActivos.size();
+                   salidaComunicacion.writeInt(numUsers);
+                   for(int i=0;i<numUsers;i++)
+                      salidaComunicacion.writeUTF(clientesActivos.get(i).nameUser);
+                   break;
+                case 3: // envia mensage a uno solo
+                   amigo=entradaComunicacion.readUTF();//captura nombre de amigo
+                   mencli=entradaComunicacion.readUTF();//mensage enviado
+                   enviaMsg(amigo,mencli);
+                   break;
+             }
+          }
+          catch (IOException e) {System.out.println("El cliente termino la conexion");break;}
+    	}
+    	serv.mostrar("Se removio un usuario");
+    	clientesActivos.removeElement(this);
+    	try
+    	{
+          serv.mostrar("Se desconecto un usuario");
+          skClienteComunicacion.close();
+    	}	
+        catch(Exception et)
+        {serv.mostrar("no se puede cerrar el socket");}   
+     }
+     
+     public void enviaMsg(String mencli2)
+     {
+        threadServidor user=null;
+        for(int i=0;i<clientesActivos.size();i++)
+        {
+           serv.mostrar("MENSAJE DEVUELTO:"+mencli2);
+           try
+            {
+              user=clientesActivos.get(i);
+              user.salida2.writeInt(1);//opcion de mensage 
+              user.salida2.writeUTF(""+this.getNameUser()+" >"+ mencli2);              
+            }catch (IOException e) {e.printStackTrace();}
+        }
+     }
+     public void enviaUserActivos()
+     {
+        threadServidor user=null;
+        for(int i=0;i<clientesActivos.size();i++)
+        {           
+           try
+            {
+              user=clientesActivos.get(i);
+              if(user==this)continue;//ya se lo envie
+              user.salida2.writeInt(2);//opcion de agregar 
+              user.salida2.writeUTF(this.getNameUser());	
+            }catch (IOException e) {e.printStackTrace();}
+        }
+     }
+
+   private void enviaMsg(String amigo, String mencli) 
+   {
+      threadServidor user=null;
+        for(int i=0;i<clientesActivos.size();i++)
+        {           
+           try
+            {
+              user=clientesActivos.get(i);
+              if(user.nameUser.equals(amigo))
+              {
+                 user.salida2.writeInt(3);//opcion de mensage amigo   
+                 user.salida2.writeUTF(this.getNameUser());
+                 user.salida2.writeUTF(""+this.getNameUser()+">"+mencli);
+              }
+            }catch (IOException e) {e.printStackTrace();}
+        }
+   }
+
+}
---
+++ Entrega2/Servidor/LogicaNegocioServidor/src/SourceLogicaNegocioServidor/LogServidor.java
@@ -0,0 +1,70 @@
+
+package SourceLogicaNegocioServidor;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ *
+ * @author acactown
+ */
+public class LogServidor {
+    
+    private static PrintWriter escritor = null;
+    private static Calendar fecha;
+    private static List<String> dias = new ArrayList<String>();    
+    private static List<String> meses = new ArrayList<String>();
+    
+    public static void setSalida (){
+        dias.add("Lunes");dias.add("Martes");dias.add("Miercoles");dias.add("Jueves");dias.add("Viernes");dias.add("Sabado");dias.add("Domingo");        
+        meses.add("Enero");meses.add("Febrero");meses.add("Marzo");meses.add("Abril");meses.add("Mayo");meses.add("Junio");meses.add("Julio");meses.add("Agosto");meses.add("Septiembre");meses.add("Octubre");meses.add("Noviembre");meses.add("Diciembre");
+        String nombreLog = "Log.log"; 
+        String ip = "N/A",host="N/A";
+        try {
+            escritor = new PrintWriter(new FileWriter(nombreLog,true));
+            InetAddress ipservidor = InetAddress.getLocalHost();
+            ip = String.valueOf(ipservidor);
+            host = ipservidor.getHostName();
+        } catch (IOException io) {
+            System.out.println("Error :  al intentar abrir el archivo : "+nombreLog +
+                                         "\n no se puede crear el Log del Servidor");
+        }finally{
+            escritor.println("\n------------------------| LOG DE SERVIDOR |-------------------" +
+                             "\n  IP del Servidor : " + ip +
+                             "\n  Nombre del Host : "+ host +
+                             "\n  Fecha y Hora de Inicio : "+ tiempoEvento() +
+                             "\n--------------------------------------------------------------");
+            escritor.flush();
+        }
+    }
+    
+    public static void setEvento(String evento) {
+        escritor.println("\n"+ tiempoEvento() +" -->"+ evento);
+        System.out.println(tiempoEvento() + evento);
+        escritor.flush();
+    }
+    
+    private static  String tiempoEvento(){   
+        fecha = Calendar.getInstance();
+        String dia = dias.get(fecha.get(Calendar.DAY_OF_WEEK) -1 );
+        String diaMes = tiempoExacto(fecha.get(Calendar.DAY_OF_MONTH) );
+        String mes = meses.get(fecha.get(Calendar.MONTH)-1 );
+        String anio = tiempoExacto(fecha.get(Calendar.YEAR) );
+        String horas = tiempoExacto(fecha.get(Calendar.HOUR_OF_DAY) );
+        String minutos = tiempoExacto(fecha.get(Calendar.MINUTE) );
+        String segundos = tiempoExacto(fecha.get( Calendar.SECOND) );
+        return dia +" "+ diaMes +"/"+ mes +"/"+ anio +"  <"+horas +":"+ minutos +":"+ segundos +">";
+    }
+    
+    private static String tiempoExacto(int unidadTiempo){
+        if( unidadTiempo < 10 ) return "0"+ String.valueOf(unidadTiempo);
+        return String.valueOf(unidadTiempo);
+    }
+    
+
+}
