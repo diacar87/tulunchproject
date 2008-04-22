99fd9d71d1f17548a69227af58bca3db9fb6d864
diacar87 <diacar87@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-22 23:38:07 +0000

    Servidor.LogicaNegocio: Se agrega clase BuscarPorCampo

---
+++ Entrega2/Servidor/LogicaNegocioServidor/src/logicaNegocio/BuscarPorCampo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logicaNegocio;
+
+import accesoDatos.dao.DaoEmpleado;
+import accesoDatos.vo.Empleado;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Yohan
+ */
+public class BuscarPorCampo {
+    public static List<Empleado> buscarEmpleado(String busqueda, String campo){
+        List<Empleado> empleados=new ArrayList<Empleado>();
+        List<Empleado> encontrados=new ArrayList<Empleado>();
+        empleados=DaoEmpleado.readAll();
+        
+         for(int i = 0 ; i< empleados.size() ; i++){
+             if(campo.equals("id") && empleados.get(i).getId().equals(busqueda)){
+                 encontrados.add(empleados.get(i));
+             }
+             if(campo.equals("nombre") && empleados.get(i).getNombre().equals(busqueda)){
+                 encontrados.add(empleados.get(i));
+             }
+             if(campo.equals("cargo") && empleados.get(i).getCargo().equals(busqueda)){
+                 encontrados.add(empleados.get(i));
+             }
+             if(campo.equals("telefono") && empleados.get(i).getTelefono().equals(busqueda)){
+                 encontrados.add(empleados.get(i));
+             }
+             if(campo.equals("celular") && empleados.get(i).getCelular().equals(busqueda)){
+                 encontrados.add(empleados.get(i));
+             }                 
+         }
+        return encontrados;                              
+    }    
+
+}
