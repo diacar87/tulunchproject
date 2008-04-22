810d78b9eb7d350d5aca1c263348fc4526d210c8
diacar87 <diacar87@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-22 23:25:16 +0000

    Se arregla persistence.xml, Se modifica metodo ValidarPassword

--- Entrega2/Servidor/LogicaNegocioServidor/src/logicaNegocio/ValidarPassword.java
+++ Entrega2/Servidor/LogicaNegocioServidor/src/logicaNegocio/ValidarPassword.java
@@ -7,6 +7,7 @@ package logicaNegocio;
 import java.util.List;
 import accesoDatos.vo.Empleado;
 import accesoDatos.dao.DaoEmpleado;
+import accesoDatos.util.Encriptacion;
 
 /**
  *
@@ -16,14 +17,13 @@ public class ValidarPassword {
     public void validarPassword(){}
     
      public static Empleado setPassword(String password){
-         List<Empleado> empleados =DaoEmpleado.readAll();        
-        for(int i=0; i<empleados.size(); i++){             
-            if (empleados.get(i).getPassword().equals(password)){               
+        List<Empleado> empleados =DaoEmpleado.readAll();        
+        for(int i=0; i<empleados.size(); i++){                 
+            if (empleados.get(i).getPassword().equals(Encriptacion.md5(password))){               
                 return empleados.get(i);
             }
         }  
-         
         return null;        
-   }
+   }     
 
 }
