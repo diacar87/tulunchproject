b2baf947164be2fca8a7217de2f88a902946ce7e
yohan8829 <yohan8829@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-25 03:49:28 +0000

    Servidor.Presentacion.util.Pantalla: Se añade el metodo "centrar"

--- Entrega2/Servidor/PresentacionServidor/src/presentacion/util/Pantalla.java
+++ Entrega2/Servidor/PresentacionServidor/src/presentacion/util/Pantalla.java
@@ -5,6 +5,8 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Window;
 import accesoDatos.util.Log;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  *  Clase que maneja posicion y tamaño de ventanas.
@@ -28,5 +30,25 @@ public class Pantalla {
         catch(Throwable ex) {
             Log.setEvento("Servidor","ERROR",ex.getMessage());
         }
+    }
+
+       /**
+     * Metodo que recibe una ventana y la centra vertical y horizontalmente en la pantalla
+     * Queneralmente corre desde una ventana entonces se ejecuta con el argumento this.
+     * @param ventana Ventana que se va a centrar
+     */
+    private void centrar(Window ventana){
+        ventana.pack();
+
+        // Se obtienen las dimensiones en pixels de la pantalla.
+        Dimension dimensionPantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        // Se obtienen las dimensiones en pixels de la ventana.
+        Dimension dimensionVentana = ventana.getSize();
+        
+        // Una cuenta para situar la ventana en el centro de la pantalla.
+        ventana.setLocation(
+            (dimensionPantalla.width - dimensionVentana.width) / 2,
+            (dimensionPantalla.height - dimensionVentana.height) / 2);
     }
 }
