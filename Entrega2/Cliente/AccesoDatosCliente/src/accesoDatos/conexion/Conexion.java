584a3a7a32e3a48e96330bb4c727d8f5da87d5f5
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-26 19:16:33 +0000

    Cliente.AccesoDatos.Conexion: Se implemnta el abrir conexion y cerrar Conexion

--- Entrega2/Cliente/AccesoDatosCliente/src/accesoDatos/conexion/Conexion.java
+++ Entrega2/Cliente/AccesoDatosCliente/src/accesoDatos/conexion/Conexion.java
@@ -23,6 +23,9 @@ public class Conexion {
     private static String ipServidor;
     private static int puertoPeticion;
     private static boolean datos;
+    private static Socket skPeticion;
+    private static ObjectInputStream entradaPeticion = null;
+    private static ObjectOutputStream salidaPeticion = null;
     
     /**
      * Metodo que se encarga de traer los datos de la conexion alamacenados en el dico duro.
@@ -44,39 +47,53 @@ public class Conexion {
             Log.setEvento("Cliente","ERROR",e.getMessage());
         }
     }
+    
+    public static boolean abrirConexion(){
+        obtenerPropiedadesConexion();
+        try {
+            skPeticion = new Socket(InetAddress.getByName( ipServidor ), puertoPeticion );
+            Log.setEvento("Cliente","INFO","Conectado a "+ skPeticion.getInetAddress().getHostAddress() + " por el puerto " + puertoPeticion );
+            salidaPeticion = new ObjectOutputStream(skPeticion.getOutputStream());
+            salidaPeticion.flush();
+            entradaPeticion = new ObjectInputStream(skPeticion.getInputStream());
+            return true;
+        } catch (UnknownHostException ex) {
+            Log.setEvento("Cliente","ERROR", ex.getMessage());
+        } catch (IOException ex) {
+            Log.setEvento("Cliente","ERROR", ex.getMessage());
+        }
+        return false;
+    }
     /**
      * Metodo que Obtiene las propiedaes de la conexion y luego envia una Peticiones al Servidor
      * @param peticion la Peticion a enviar
      * @return El objeto que retorna el Servidor, pilas hay que hacerle el cast
      */
     public static Object enviarPeticion(Peticion peticion){
-        if(!datos){
-            obtenerPropiedadesConexion();
-            datos = true;
-        }
-        Socket skPeticion;
-        ObjectInputStream entradaPeticion = null;
-        ObjectOutputStream salidaPeticion = null;
         try {
-            Log.setEvento("Cliente","INFO","Intentando Conectarse al Servidor con IP "+ ipServidor +" por el puerto " + puertoPeticion);
-            skPeticion = new Socket(InetAddress.getByName( ipServidor ), puertoPeticion );
-            Log.setEvento("Cliente","INFO","Conectado a "+ skPeticion.getInetAddress().getHostName() );
-            Log.setEvento("Cliente","INFO","Se envian Flujos de Entrada y Salida al Servidor");
-            salidaPeticion = new ObjectOutputStream(skPeticion.getOutputStream());
-            salidaPeticion.flush();
-            entradaPeticion = new ObjectInputStream(skPeticion.getInputStream());
-            Log.setEvento("Cliente","INFO","Se envia Peticion");
+            Log.setEvento("Cliente","INFO","Se envia Peticion al Servidor.");
             salidaPeticion.writeObject(peticion);
             salidaPeticion.flush();
             return entradaPeticion.readObject();
-        } catch (UnknownHostException ex) {
-            Log.setEvento("Cliente","ERROR", ex.getMessage());
         } catch (IOException ex) {
             Log.setEvento("Cliente","ERROR", ex.getMessage());
         } catch (ClassNotFoundException ex) {
             Log.setEvento("Cliente","WARNING", ex.getMessage());
         }
         return null;
+    }
+    
+    public static boolean cerrarConexion(){
+        try {
+            salidaPeticion.close();
+            entradaPeticion.close();
+            skPeticion.close();
+            Log.setEvento("Cliente","INFO","Se Desconecta del Servidor.");
+            return true;
+        } catch (IOException ex) {
+            Log.setEvento("Cliente","WARNING",ex.getMessage());
+        }   
+        return false;
     }
     
 }
