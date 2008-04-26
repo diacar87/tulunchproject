941dc526655801dd359512fa498767884410e621
acactown <acactown@8b7e5a1b-7c4a-0410-9bc1-91ef6b37bc24>
2008-04-26 15:51:49 +0000

    Servidor.LogicaNegocio.Servidcios: puede que una clase se llame igual y se completamente igual a otra, pero si estan en diferentes paquetes esto ya las hace diferentes asi sean completamente iguales en codigo.

---
+++ Entrega2/Servidor/LogicaNegocioServidor/src/logicaNegocio/servicios/HiloCliente.java
@@ -0,0 +1,67 @@
+
+package logicaNegocio.servicios;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import accesoDatos.util.Log;
+
+/**
+ *
+ * @author acactown
+ */
+public class HiloCliente implements Runnable{
+
+    private Socket skPeticion;
+    private ObjectInputStream entradaPeticion;
+    private ObjectOutputStream salidaPeticion;
+    private boolean atendiendo;
+    private Peticion peticion;
+    
+    public HiloCliente(Socket skPeticion) {
+        this.skPeticion = skPeticion;
+        entradaPeticion = null;
+        salidaPeticion = null;
+        atendiendo = true;
+    }
+        
+    public void run(){
+        try {
+            Log.setEvento("Servidor","INFO","Se envian Flujos de Entrada/Salida al Cliente en el Hilo<p class=\"salto\">"+ this.toString() +"</p>");
+            salidaPeticion = new ObjectOutputStream(skPeticion.getOutputStream());
+            salidaPeticion.flush();
+            entradaPeticion = new ObjectInputStream(skPeticion.getInputStream());
+            Log.setEvento("Servidor","INFO","Esperando Peticiones de Cliente en el Hilo<p class=\"salto\">"+ this.toString() +"</p>");
+        } catch (IOException ex) {
+            Log.setEvento("Servidor","ERROR",ex.getMessage());
+        }
+        while( atendiendo ){
+            try {
+                peticion = (Peticion)entradaPeticion.readObject();
+                switch (peticion.getOpcion()) {
+                    case 1:
+                        peticion.setNombreCliente("Camilo");
+                        salidaPeticion.writeObject(peticion);
+                        salidaPeticion.flush();
+                    break;
+                }
+            } catch (IOException ex) {
+                /*Log.setEvento("Servidor","INFO","El Cliente Termino la Conexion desde IP : "+ skPeticion.getInetAddress().getHostAddress() +
+                                            "<p class=\"salto\">Empleado    : "+ peticion.getEmpleado().getNombre() +"</p>" +
+                                            "<p class=\"salto\">Cargo       : "+ peticion.getEmpleado().getCargo() +"</p>" +
+                                            "<p class=\"salto\">Por Hilo    : "+ this.toString() +"</p>"); */
+                break;
+            } catch (ClassNotFoundException ex) {
+                Log.setEvento("Servidor","WARNING","Entrada Desconocida , "+ ex.getMessage());
+                continue;
+            }
+        }
+        try{
+            skPeticion.close();
+        } catch (IOException ex) {
+            Log.setEvento("Servidor","ERROR",ex.getMessage());
+        }
+    }
+    
+}
